51220aa64c9d8f6115d5e29886ac7f9e0627ff86
Karena Stevens <kaymark09@gmail.com>
2016-09-12 08:07:36 -0700

    Complete advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
