cd053219540a12c57bbd016c3b2210ad85e2e997
Karena Stevens <kaymark09@gmail.com>
2016-12-31 11:59:53 -0800

    finish contact form and mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
-  get '/contact',  to: 'static_pages#contact'
-  get '/forms',    to: 'static_pages#forms'
-  get '/about',    to: 'static_pages#about'
+  get  '/contact',  to: 'messages#new'
+  post '/contact',  to: 'messages#create'
+  get  '/forms',    to: 'static_pages#forms'
+  get  '/about',    to: 'static_pages#about'
+  resources :messages, only: [:new, :create]
 end
