3237a4f6f21f03e6d08cc35441b6026563c0d625
Suvigya <suvi7896@gmail.com>
2017-08-21 19:21:00 +0530

    google verification file route added

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post '/contact',  to: 'messages#create'
   get  '/forms',    to: 'static_pages#forms'
   get  '/about',    to: 'static_pages#about'
+  get  '/googlecb4b52e84bc8ad0b', to: 'static_pages#googlecb4b52e84bc8ad0b'
   resources :messages, only: [:new, :create]
 end
