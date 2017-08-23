aa22aa458389e362f21894719c21e287b61cea8a
Suvigya <suvi7896@gmail.com>
2017-08-23 14:48:24 +0530

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do
 
+Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/contact',  to: 'messages#new'
   post '/contact',  to: 'messages#create'
   get  '/forms',    to: 'static_pages#forms'
   get  '/about',    to: 'static_pages#about'
-  get  '/googlecb4b52e84bc8ad0b', to: 'static_pages#'
+
+
   resources :messages, only: [:new, :create]
 end
