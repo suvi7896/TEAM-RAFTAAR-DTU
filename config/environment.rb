cd12303019020be710d0d154d3fe785b2cedf7de
Suvigya <suvi7896@gmail.com>
2017-08-23 17:57:14 +0530

    smtp config added

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,19 @@
 # Load the Rails application.
 require_relative 'application'
 
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :address              => "",
+    :port                 => 587,
+    :domain               => "",
+    :user_name            => "",
+    :password             => "",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+}
+
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
