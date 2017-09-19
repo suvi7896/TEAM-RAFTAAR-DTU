68ff6be792caf979b12138f78c19017ce6ed5dd8
Suvigya <suvi7896@gmail.com>
2017-09-20 03:47:01 +0530

    Contact Form Mailer Functional

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module PlayfulMinds
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
   end
 end
