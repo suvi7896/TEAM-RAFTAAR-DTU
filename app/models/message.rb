d476a618c6052cc1766843e7eef397c2a96df0b8
Suvigya <suvi7896@gmail.com>
2017-09-20 03:50:40 +0530

    Contact Form Mailer Functional

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
-class Message 
+class Message
   include ActiveModel::Model
   attr_accessor :name, :email, :subject, :content
   validates :name, presence: true, length: { maximum: 50 }
@@ -8,3 +8,19 @@ class Message
   validates :subject, presence: true, length: { maximum: 300 }
   validates :content, presence: true
 end
+
+                   # attribute :name,      :validate => true
+                   # attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+                   # attribute :message
+                   # attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+                   # def headers
+                   #   {
+                     #     :subject => "My Contact Form",
+                   #    :to => "your_email@example.org",
+                      #    :from => %("#{name}" <#{email}>)
+                    #  }
+                   # end
+#end
