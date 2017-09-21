bb07441366d5491da25aa4165950db47d90379cd
Suvigya <suvi7896@gmail.com>
2017-09-21 17:23:01 +0530

    Live website mailer bug resolved

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require 'socket'
+require 'ipaddr'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,7 +15,9 @@ module PlayfulMinds
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-
+    config.web_console.whitelisted_ips = Socket.ip_address_list.reduce([]) do |res, addrinfo|
+      addrinfo.ipv4? ? res << IPAddr.new(addrinfo.ip_address).mask(24) : res
+  end
     config.web_console.whitelisted_ips = ' 59.177.137.239'
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
